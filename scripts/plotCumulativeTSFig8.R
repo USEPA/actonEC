9f7cbbcd2ad0a97d9bee7e67b8d13dd1bb639013
Sarah McNabb Waldo <swaldo@Sarahs-MacBook-Pro.local>
2020-06-05 10:16:05 -0700

    updated this script for Figure 8; now have a figure with all traces, and added data to scihub.

---
+++ scripts/plotCumulativeTSFig8.R
@@ -0,0 +1,250 @@
+
+######## Prep the EC Data: gapfilled, upper and lower confidence intervals #####################
+
+ggplot(DailyANNFluxes, aes(date, ch4.t_cumu))+
+  geom_line()
+
+
+
+rowNum<-which(grepl("2018-01-01", DailyANNFluxes$date))
+value17<-DailyANNFluxes$ch4.t_cumu[rowNum]
+value17L<-DailyANNFluxes$ch4.t_cumuL95[rowNum]
+value17U<-DailyANNFluxes$ch4.t_cumuU95[rowNum]
+
+#break it up by year: subtract max 2017 cumulative value from 2018 to start at 0 
+for(i in 1:nrow(DailyANNFluxes)){
+  DailyANNFluxes$ch4.t_cumu_yr[i]<-ifelse(DailyANNFluxes$year[i]<2018,
+                                          DailyANNFluxes$ch4.t_cumu[i],
+                                          DailyANNFluxes$ch4.t_cumu[i] - value17)
+  DailyANNFluxes$ch4.t_cumu_yr_U95[i]<-ifelse(DailyANNFluxes$year[i]<2018,
+                                              DailyANNFluxes$ch4.t_cumuU95[i],
+                                              DailyANNFluxes$ch4.t_cumuU95[i] - value17U)
+  DailyANNFluxes$ch4.t_cumu_yr_L95[i]<-ifelse(DailyANNFluxes$year[i]<2018,
+                                              DailyANNFluxes$ch4.t_cumuL95[i],
+                                              DailyANNFluxes$ch4.t_cumuL95[i] - value17L)
+}
+
+
+######### AFT cumulative areal emissions and error -----
+### The AFT uncertainty is the standard deviation, not confidence interval
+### but the script names it CI so it can be coerced into a long data frame with the 
+### EC and GRTS data for plotting
+
+### Need to sum daily errors in quadrature:
+DailyDeepTfluxes$ch4.cumuErr<-DailyDeepTfluxes$dailyCH4TErr
+
+for(i in 2:nrow(DailyDeepTfluxes)){
+  DailyDeepTfluxes$ch4.cumuErr[i]<-ifelse(!is.na(DailyDeepTfluxes$dailyCH4TErr[i]),
+                                          (sqrt(DailyDeepTfluxes$ch4.cumuErr[i-1]^2+
+                                                  DailyDeepTfluxes$dailyCH4TErr[i]^2)),
+                                          DailyDeepTfluxes$ch4.cumuErr[i-1])
+}
+DailyDeepTfluxes$ch4.cumuErr<-DailyDeepTfluxes$ch4.cumuErr*24/1000
+
+DailyDeepTfluxes<-mutate(DailyDeepTfluxes,
+                         ch4.trate_zero = replace(ch4.trate, is.na(ch4.trate), 0), #zero means that NAs have been replaced with zeros
+                         ch4.t.cumu = cumsum(ch4.trate_zero)*24/1000, #convert from mg/m2/hr to g/m2/d
+                         #ch4.cumuErr = cumsum(dailyCH4TErr)*24/1000, need to add in quadrature
+                         ch4.t.cumuUCI = ch4.t.cumu+ch4.cumuErr, 
+                         ch4.t.cumuLCI = ch4.t.cumu-ch4.cumuErr)
+
+DailyDeepTfluxes<-DailyDeepTfluxes%>%
+  mutate(ch4.t_cumu_yr = ch4.t.cumu,
+         ch4.t_cumu_yr = replace(ch4.t_cumu_yr, monthday<"2020-05-01", NA),
+         ch4.t_cumuL95=ch4.t_cumu_yr-ch4.cumuErr,
+         ch4.t_cumuU95=ch4.t_cumu_yr+ch4.cumuErr
+  )
+#take care of subtracting 2017 from 2018
+rowNum<-which(grepl("2018-01-01", DailyDeepTfluxes$date))
+value17<-DailyDeepTfluxes$ch4.t.cumu[rowNum]
+value17L<-DailyDeepTfluxes$ch4.t.cumuLCI[rowNum]
+value17U<-DailyDeepTfluxes$ch4.t.cumuUCI[rowNum]
+
+for(i in 1:nrow(DailyDeepTfluxes)){
+  DailyDeepTfluxes$ch4.t_cumu_yr[i]<-ifelse(DailyDeepTfluxes$year[i]<2018,
+                                            DailyDeepTfluxes$ch4.t.cumu[i],
+                                            DailyDeepTfluxes$ch4.t.cumu[i] - value17)
+  DailyDeepTfluxes$ch4.t_cumu_yr_U95[i]<-ifelse(DailyDeepTfluxes$year[i]<2018,
+                                              DailyDeepTfluxes$ch4.t.cumuUCI[i],
+                                              DailyDeepTfluxes$ch4.t.cumuUCI[i] - value17U)
+  DailyDeepTfluxes$ch4.t_cumu_yr_L95[i]<-ifelse(DailyDeepTfluxes$year[i]<2018,
+                                              DailyDeepTfluxes$ch4.t.cumuLCI[i],
+                                              DailyDeepTfluxes$ch4.t.cumuLCI[i] - value17L)
+}
+
+ggplot(DailyDeepTfluxes, aes(Rdate, ch4.t_cumu_yr))+
+  geom_line()+
+  geom_ribbon(data=DailyDeepTfluxes,
+              aes(x=Rdate, ymin=ch4.t_cumu_yr_L95,
+              ymax=ch4.t_cumu_yr_U95), alpha=0.3)
+
+######### shallow AFT + Cham site -----
+### Need to sum daily errors in quadrature:
+DailyShalTfluxes$ch4.cumuErr<-DailyShalTfluxes$dailyCH4TErr
+
+for(i in 2:nrow(DailyShalTfluxes)){
+  DailyShalTfluxes$ch4.cumuErr[i]<-ifelse(!is.na(DailyShalTfluxes$dailyCH4TErr[i]),
+                                          (sqrt(DailyShalTfluxes$ch4.cumuErr[i-1]^2+
+                                                 DailyShalTfluxes$dailyCH4TErr[i]^2)),
+                                          DailyShalTfluxes$ch4.cumuErr[i-1])
+}
+DailyShalTfluxes$ch4.cumuErr<-DailyShalTfluxes$ch4.cumuErr*24/1000
+
+DailyShalTfluxes<-mutate(DailyShalTfluxes,
+                         ch4.trate_zero = replace(ch4.trate, is.na(ch4.trate), 0), #zero means that NAs have been replaced with zeros
+                         ch4.t.cumu = cumsum(ch4.trate_zero)*24/1000, #convert from mg/m2/hr to g/m2/d
+                         #ch4.cumuErr = cumsum(dailyCH4TErr)*24/1000, need to add in quadrature
+                         ch4.t.cumuUCI = ch4.t.cumu+ch4.cumuErr, 
+                         ch4.t.cumuLCI = ch4.t.cumu-ch4.cumuErr)
+
+DailyShalTfluxes<-DailyShalTfluxes%>%
+  mutate(ch4.t_cumu_yr = ch4.t.cumu,
+         ch4.t_cumu_yr = replace(ch4.t_cumu_yr, monthday<"2020-05-01", NA),
+         ch4.t_cumuL95=ch4.t_cumu_yr-ch4.cumuErr,
+         ch4.t_cumuU95=ch4.t_cumu_yr+ch4.cumuErr
+  )
+#take care of subracting 2017 from 2018
+rowNum<-which(grepl("2018-01-01", DailyShalTfluxes$date))
+value17<-DailyShalTfluxes$ch4.t.cumu[rowNum]
+value17L<-DailyShalTfluxes$ch4.t.cumuLCI[rowNum]
+value17U<-DailyShalTfluxes$ch4.t.cumuUCI[rowNum]
+
+for(i in 1:nrow(DailyShalTfluxes)){
+  DailyShalTfluxes$ch4.t_cumu_yr[i]<-ifelse(DailyShalTfluxes$year[i]<2018,
+                                            DailyShalTfluxes$ch4.t.cumu[i],
+                                            DailyShalTfluxes$ch4.t.cumu[i] - value17)
+  DailyShalTfluxes$ch4.t_cumu_yr_U95[i]<-ifelse(DailyShalTfluxes$year[i]<2018,
+                                                DailyShalTfluxes$ch4.t.cumuUCI[i],
+                                                DailyShalTfluxes$ch4.t.cumuUCI[i] - value17U)
+  DailyShalTfluxes$ch4.t_cumu_yr_L95[i]<-ifelse(DailyShalTfluxes$year[i]<2018,
+                                                DailyShalTfluxes$ch4.t.cumuLCI[i],
+                                                DailyShalTfluxes$ch4.t.cumuLCI[i] - value17L)
+}
+
+# ggplot(DailyShalTfluxes, aes(Rdate, ch4.t_cumu_yr))+
+#   geom_line()+
+#   geom_ribbon(data=DailyShalTfluxes,
+#               aes(x=Rdate, ymin=ch4.t_cumu_yr_L95,
+#               ymax=ch4.t_cumu_yr_U95, alpha=0.2))
+
+############# GRTS data ####
+
+#total
+grts_ts<-grts_ts%>%
+  mutate(ch4.t_gf = replace(ch4.t_gf, date>"2017-05-01" & date<"2017-07-10", grts_ts$ch4.t_gf[which(grepl("2017-07-10", grts_ts$date))]),
+         ch4.t_gf = replace(ch4.t_gf, date>"2018-05-01" & date<"2018-07-10", grts_ts$ch4.t_gf[which(grepl("2018-07-10", grts_ts$date))]),
+         ch4.t_gf = replace(ch4.t_gf, date>"2018-09-19" & date<"2018-10-01", grts_ts$ch4.t_gf[which(grepl("2018-09-19", grts_ts$date))]),
+         ch4.t_gf = replace(ch4.t_gf, date<"2017-05-01", 0),
+         ch4.t_gf = replace(ch4.t_gf, date>"2017-10-04" & date<"2018-05-01", 0),
+         ch4.t_gf = replace(ch4.t_gf, date>"2018-10-01", 0),
+         ch4.t_gf = na.approx(ch4.t_gf, rule=2),
+         ch4.t_cumu = cumsum(ch4.t_gf)/1000*24,
+         ch4.t_cumuPt = as.numeric(NA),
+         ch4.t_cumuErr = as.numeric(NA))
+
+for(i in 1:nrow(grts_ts)){
+  grts_ts$ch4.t_cumuPt[i] <- ifelse(is.na(grts_ts$ch4.trate.FE[i]),
+                                    grts_ts$ch4.t_cumuPt[i],
+                                    grts_ts$ch4.t_cumu[i])
+  grts_ts$ch4.t_cumuErr[i]<- ifelse(is.na(grts_ts$ch4.trate.FE[i]),
+                                    grts_ts$ch4.t_cumuErr[i],
+                                    grts_ts$ch4.t_cumu[i]*grts_ts$ch4.trate.FE[i])
+}
+
+grts_ts<-grts_ts%>%
+  mutate(year = year(date),
+         monthday = format(date, format="%m-%d %H:%M"))
+
+
+#####Test plots, cuml calcs#
+ggplot(grts_ts, aes(date, ch4.trate.mg.h_Estimate))+
+  geom_point()+
+  geom_point(aes(date, ch4.erate.mg.h_Estimate), color="red")+
+  geom_point(aes(date, ch4.drate.mg.m2.h_Estimate), color = "blue")+
+  geom_line(aes(date, ch4.t_gf))+
+  geom_line(aes(date, ch4.e_gf), color="red")+
+  geom_line(aes(date, ch4.d_gf), color="blue")+
+  ylim(0, 50)
+
+ggplot(grts_ts, aes(date, ch4.t_cumu))+
+  geom_line()+
+  geom_errorbar(data=grts_ts, aes(date, grts_ts$ch4.t_cumuPt), ymin=ch4.t_cumuPt-ch4.t_cumuErr,
+                ymax=ch4.t_cumuPt+ch4.t_cumuErr)
+
+ggplot(grts_ts, aes(date, ch4.t_cumuPt))+
+  geom_point()+
+  geom_errorbar(data = grts_ts,
+                aes(ymin=(ch4.t_cumuPt-ch4.t_cumuErr),
+                    ymax=(ch4.t_cumuPt+ch4.t_cumuErr)))
+
+GRTSF8<-grts_ts%>%
+  mutate(ch4.t_gf = replace(ch4.t_gf, date<"2017-05-01", NA),
+         ch4.t_gf = replace(ch4.t_gf, date>"2017-10-04" & date<"2018-05-01", NA),
+         ch4.t_gf = replace(ch4.t_gf, date>"2018-10-01", NA),
+         ch4.trate = ch4.t_gf,
+         ch4.tPt = ch4.t_cumuPt,
+         ch4.FE = ch4.trate.FE,
+         Label = "d) Lake-Wide Surveys",
+         year = year(date),
+         monthday = format(date, format="%m-%d %H:%M"))# %>%
+GRTSF8$monthday<-as.Date(GRTSF8$monthday, format="%m-%d %H:%M")
+
+rowNum<-which(grepl("2018-01-01", GRTSF8$date))
+value17<-GRTSF8$ch4.t_cumu[rowNum]
+
+for(i in 1:nrow(GRTSF8)){
+  GRTSF8$ch4.t_cumu_yr[i]<-ifelse(GRTSF8$year[i]<2018,
+                                  GRTSF8$ch4.t_cumu[i],
+                                  GRTSF8$ch4.t_cumu[i] - value17)
+  GRTSF8$ch4.tPt_yr[i]<-ifelse(GRTSF8$year[i]<2018,
+                               GRTSF8$ch4.tPt[i],
+                               GRTSF8$ch4.tPt[i] - value17)
+  GRTSF8$ch4.tPt_err[i]<-GRTSF8$ch4.tPt_yr[i]*GRTSF8$ch4.FE[i]
+  
+}
+
+GRTSF8<-GRTSF8%>%
+  mutate(ch4.t_cumu_yr = replace(ch4.t_cumu_yr, monthday<"2020-05-01", NA),
+         ch4.t_cumu_yr = replace(ch4.t_cumu_yr, monthday>"2020-10-04", NA),
+         ch4.t_cumu_yr_L95 = ch4.tPt_yr-ch4.tPt_err,
+         ch4.t_cumu_yr_U95 = ch4.tPt_yr+ch4.tPt_err
+  )
+
+DailyANNF8<-select(DailyANNFluxes, date, Label, year, ch4.t_cumu, monthday, ch4.t_cumu_yr, ch4.t_cumu_yr_U95, ch4.t_cumu_yr_L95)
+ShalSiteF8<-select(DailyShalTfluxes, date, Label, year, ch4.t_cumu, monthday, ch4.t_cumu_yr, ch4.t_cumu_yr_U95, ch4.t_cumu_yr_L95)
+DeepSiteF8<-select(DailyDeepTfluxes, date, Label, year, ch4.t_cumu, monthday, ch4.t_cumu_yr, ch4.t_cumu_yr_U95, ch4.t_cumu_yr_L95)
+GRTSF8<-select(GRTSF8, date, Label, year, ch4.t_cumu, monthday, ch4.t_cumu_yr, ch4.t_cumu_yr_U95, ch4.t_cumu_yr_L95)
+
+cumulativeFluxF8<-list()
+cumulativeFluxF8[[1]]<-DailyANNF8
+cumulativeFluxF8[[2]]<-ShalSiteF8
+cumulativeFluxF8[[3]]<-DeepSiteF8
+cumulativeFluxF8[[4]]<-GRTSF8
+
+cmlDailyFluxF8<-do.call("rbind", cumulativeFluxF8)
+
+write.csv(cmlDailyFluxF8, paste0(projectWD, "/dataL2/Fig8data.csv"),
+          row.names =FALSE)
+
+#Figure 8:
+ggplot(filter(cmlDailyFluxF8, year>2016), aes(monthday, ch4.t_cumu_yr))+
+  geom_line(aes(color=Label, linetype=Label), size=1)+
+  geom_ribbon(data=filter(cmlDailyFluxF8, year>2016), 
+              aes(x = monthday, 
+                  ymin = ch4.t_cumu_yr_L95,
+                  ymax = ch4.t_cumu_yr_U95,
+                  color = Label, fill=Label),alpha=0.2,
+              show.legend=FALSE)+
+  facet_grid(.~year)+
+  scale_x_date(date_minor_breaks = "1 month", 
+               labels = date_format("%b"))+
+  theme(legend.position = "top",
+        legend.title=element_blank())+
+  #theme_bw()+
+  ylab(expression(Cumulative~CH[4]~Areal~Emissions~(g~m^-2)))+
+  xlab("")+
+  labs(fill = "Method")+
+  theme_bw()+
+  theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank(),
+        axis.line=element_line(colour="black"),legend.title=element_blank())
+
